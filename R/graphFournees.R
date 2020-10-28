82edc7e94d481a4d94279eb7b82528f7ea6a77fa
sylvainschmitt <sylvain.schmitt@agroparistech.fr>
2020-10-28 11:35:54 +0100

    rm cowplot

--- R/graphFournees.R
+++ R/graphFournees.R
@@ -2,7 +2,6 @@
 #' @import ggplot2
 #' @importFrom dplyr rename
 #' @importFrom reshape2 melt
-#' @importFrom cowplot plot_grid
 NULL
 
 #' Graphique des fournees
@@ -15,17 +14,16 @@ NULL
 #'
 #' @examples
 graphFournees <- function(pains, fournees){
-  g1 <- ggplot(pains, aes(Type, Poids, fill = Type)) +
-    geom_col() +
-    theme(legend.position = "bottom") +
-    coord_flip() +
-    geom_hline(yintercept = max(colSums(fournees)), linetype = "dashed") +
-    scale_fill_manual("Type", values = c("brown3", "beige", "yellow", "darkgrey"))
-  g2 <- melt(fournees) %>%
+  # g1 <- ggplot(pains, aes(Type, Poids, fill = Type)) +
+  #   geom_col() +
+  #   theme(legend.position = "bottom") +
+  #   coord_flip() +
+  #   geom_hline(yintercept = max(colSums(fournees)), linetype = "dashed") +
+  #   scale_fill_manual("Type", values = c("brown3", "beige", "yellow", "darkgrey"))
+  melt(fournees) %>%
     rename(Type = Var1, Fournee = Var2, Poids = value) %>%
     ggplot(aes(Fournee, Poids, fill = Type)) +
     geom_col() +
     theme(legend.position = "bottom") +
     scale_fill_manual("Type", values = c("darkgrey", "yellow", "brown3", "beige"))
-  plot_grid(g1, g2, nrow = 2)
 }
